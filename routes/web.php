96e30c4dab8c0894f82450754ad335d2b86b4960
Nicolae Plesa <nicu.plesa@gmail.com>
2025-11-10 12:34:50 +0200

    Fix namespace casing for ViewLogs import

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\\Support\\Facades\\Route;
-use Nplesa\\ViewLogs\\Http\\Livewire\\ViewLogs;
+use Illuminate\Support\Facades\Route;
+use nplesa\ViewLogs\Http\Livewire\ViewLogs;
 
 Route::middleware(config('viewlogs.middleware', ['web']))->group(function() {
     Route::get(config('viewlogs.route', '/view-logs'), ViewLogs::class);
